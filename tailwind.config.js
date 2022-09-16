f769179973880ba5402d46c3f6c68a50e1de3e4d
Romer <romer@nostal.dev>
2022-09-16 17:34:45 +0200

    Hero & Quick Actions

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      white: "#fff",
+      black: "#000",
+      red: "#ca464c",
+      yellow: "#fab102",
+      purple: "#B02996",
+      blue: "#266ea1",
+      green: "#459828",
+      orange: "#E18335",
+    },
+    fontFamily: {
+      sans: ["Noto Sans", "sans-serif"],
+      serif: ["Noto Serif", "serif"],
+    },
+  },
+  plugins: [],
+};
